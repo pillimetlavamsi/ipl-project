6a3b605f80bdf2d6e571f79053c201bc4148da0b
vamsi pillimetla <vamsipillimetla@gmail.com>
2024-07-14 00:04:41 +0530

    deploy

---
+++ javascript/login.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.querySelector('.container');
+  
+    setTimeout(function () {
+      container.classList.add('active');
+    }, 100);
+  
+    document.querySelector('#loginForm').addEventListener('submit', function (e) {
+      e.preventDefault();
+      const username = document.querySelector('#username').value;
+      const password = document.querySelector('#password').value;
+  
+      if (username === 'Kishan' && password === 'Kishan_2004') {
+        alert('Login successful!');
+        window.location.href = '/html/dashboard.html'; 
+      } else if (username === 'Vamsi' && password === 'Vamsi_2004') {
+        alert('Login successful!');
+        window.location.href = '/html/dashboard.html';
+      } else {
+        alert('Invalid username');
+      }
+    });
+  });
+  
